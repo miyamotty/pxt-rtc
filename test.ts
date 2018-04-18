083559f081b011e912f1753e1b210b25e2b74aed
Yuichi Miyamoto <yuichi@prty.jp>
2018-04-19 01:17:16 +0900

    test

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
 // tests go here; this will not be compiled when this package is used as a library
 basic.forever(() => {
-    rtc.setTimeData(18, 4, 18, 12, 0)
+   // rtc.setTimeData(18, 4, 18, 12, 0)
 })
